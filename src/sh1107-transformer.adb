beb26de0479d0d33586c8dda94b8e504b91f18ee
hrodriguez <holger.rodriguez@roseng.ch>
2021-12-18 16:22:59 +0100

    SH1107.Transformer: added Up/Down specifications

--- src/sh1107-transformer.adb
+++ src/sh1107-transformer.adb
@@ -1,43 +1,71 @@
 package body SH1107.Transformer is
 
-   function Bit_Mask_1 (Y : Natural) return HAL.UInt8;
-   function Bit_Mask_2_3 (Y : Natural) return HAL.UInt8;
-   type Bit_Mask_Function is access function (Y : Natural) return HAL.UInt8;
-   Bit_Mask_Functions : constant array (1 .. 3) of Bit_Mask_Function
-     := (1 => Bit_Mask_1'Access,
-         2 => Bit_Mask_2_3'Access,
-         3 => Bit_Mask_2_3'Access
-        );
+   function Get_Page_Up (Y : Natural) return Natural;
+   function Get_Page_Down (Y : Natural) return Natural;
 
-   function Get_Page_1_2 (Y : Natural) return Natural;
-   function Get_Page_3 (Y : Natural) return Natural;
-   type Get_Page_Function is access function (Y : Natural) return Natural;
-   Get_Page_Functions : constant array (1 .. 3) of Get_Page_Function
-     := (1 => Get_Page_1_2'Access,
-         2 => Get_Page_1_2'Access,
-         3 => Get_Page_3'Access
-        );
+   function Get_Column_Up (X : Natural) return Natural;
+   function Get_Column_Down (X : Natural) return Natural;
 
-   function Get_Column_1_2 (X : Natural) return Natural;
-   function Get_Column_3 (X : Natural) return Natural;
-   type Get_Column_Function is access function (X : Natural) return Natural;
-   Get_Column_Functions : constant array (1 .. 3) of Get_Column_Function
-     := (1 => Get_Column_1_2'Access,
-         2 => Get_Column_1_2'Access,
-         3 => Get_Column_3'Access
-        );
+   function Get_Page_Up (Y : Natural) return Natural is
+   begin
+      return Y / 8;
+   end Get_Page_Up;
 
-   ----------------------
+   function Get_Page_Down (Y : Natural) return Natural is
+   begin
+      return 15 - Y / 8;
+   end Get_Page_Down;
+
+   function Get_Column_Up (X : Natural) return Natural is
+   begin
+      return X;
+   end Get_Column_Up;
+
+   function Get_Column_Down (X : Natural) return Natural is
+   begin
+      return 127 - X;
+   end Get_Column_Down;
+
+   function Get_Byte_Index (O    : SH1107_Orientation;
+                            X, Y : Natural) return Natural is
+   begin
+      case O is
+         when Up =>
+            return 128 * Get_Page_Up (Y) + Get_Column_Up (X);
+         when Down =>
+            return 128 * Get_Page_Down (Y) + Get_Column_Down (X);
+         when others =>
+            return 0;
+      end case;
+   end Get_Byte_Index;
+
+   --========================================================================
+   --
+   --  This section is the implementation of the bit mask calculation
+   --
+   --========================================================================
+   function Bit_Mask_Up (Y : Natural) return HAL.UInt8;
+   function Bit_Mask_Down (Y : Natural) return HAL.UInt8;
 
    function Get_Bit_Mask (O    : SH1107_Orientation;
                           X, Y : Natural) return HAL.UInt8 is
-      pragma Unreferenced (O);
       pragma Unreferenced (X);
    begin
-      return Bit_Mask_Functions (SH1107.Transformer.THE_VARIANT) (Y);
+      case O is
+         when Up =>
+            return Bit_Mask_Up (Y);
+         when Down =>
+            return Bit_Mask_Down (Y);
+         when others =>
+            return 16#00#;
+      end case;
    end Get_Bit_Mask;
 
-   function Bit_Mask_1 (Y : Natural) return HAL.UInt8 is
+   --------------------------------------------------------------------------
+   --  If the orienation is Up, this means, that the Y-coordinates
+   --  map from a logical perspective:
+   --  increasing Y coordincates moves the bit mask from D0 -> D7
+   function Bit_Mask_Up (Y : Natural) return HAL.UInt8 is
       LUT : constant HAL.UInt8_Array (0 .. 7)
         := (
             0 => 16#01#,
@@ -51,9 +79,13 @@ package body SH1107.Transformer is
            );
    begin
       return LUT (Y mod 8);
-   end Bit_Mask_1;
+   end Bit_Mask_Up;
 
-   function Bit_Mask_2_3 (Y : Natural) return HAL.UInt8 is
+   --------------------------------------------------------------------------
+   --  If the orienation is Down, this means, that the Y-coordinates
+   --  map from a logical perspective:
+   --  increasing Y coordincates moves the bit mask from D7 -> D0
+   function Bit_Mask_Down (Y : Natural) return HAL.UInt8 is
       LUT : constant HAL.UInt8_Array (0 .. 7)
         := (
             0 => 16#80#,
@@ -67,34 +99,6 @@ package body SH1107.Transformer is
            );
    begin
       return LUT (Y mod 8);
-   end Bit_Mask_2_3;
-
-   function Get_Page_1_2 (Y : Natural) return Natural is
-   begin
-      return Y / 8;
-   end Get_Page_1_2;
-
-   function Get_Page_3 (Y : Natural) return Natural is
-   begin
-      return 15 - Y / 8;
-   end Get_Page_3;
-
-   function Get_Column_1_2 (X : Natural) return Natural is
-   begin
-      return X;
-   end Get_Column_1_2;
-
-   function Get_Column_3 (X : Natural) return Natural is
-   begin
-      return 127 - X;
-   end Get_Column_3;
-
-   function Get_Byte_Index (O    : SH1107_Orientation;
-                            X, Y : Natural) return Natural is
-      pragma Unreferenced (O);
-   begin
-      return 128 * Get_Page_Functions (THE_VARIANT) (Y)
-        + Get_Column_Functions (THE_VARIANT) (X);
-   end Get_Byte_Index;
+   end Bit_Mask_Down;
 
 end SH1107.Transformer;
--- src/sh1107-transformer.ads
+++ src/sh1107-transformer.ads
@@ -1,10 +1,29 @@
+--===========================================================================
+--
+--  This package is the interface to the SH1107 transfomer
+--     The transformer transforms the logical coordinates in the given
+--     orientation the corresponding indices and bits.
+--
+--===========================================================================
+--
+--  Copyright 2021 (C) Holger Rodriguez
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
+
 private package SH1107.Transformer is
 
-   THE_VARIANT : constant Integer := 1;
-
+   --------------------------------------------------------------------------
+   --  Calculates the byte index into the memory area,
+   --  which corresponds to the logical
+   --  Orientation and X, Y coordinates
    function Get_Byte_Index (O    : SH1107_Orientation;
                             X, Y : Natural) return Natural;
 
+   --------------------------------------------------------------------------
+   --  Calculates the bit mask to use
+   --  which corresponds to the logical
+   --  Orientation and X, Y coordinates
    function Get_Bit_Mask (O    : SH1107_Orientation;
                           X, Y : Natural) return HAL.UInt8;
 
