647f5db7afa09c5c80f95af022766c551ec21b62
hrodriguez <holger.rodriguez@roseng.ch>
2021-12-20 18:45:04 +0100

    SH1107.Transformer: added comments and reduced stack size by making LUT package level

--- src/sh1107-transformer.adb
+++ src/sh1107-transformer.adb
@@ -1,3 +1,14 @@
+--===========================================================================
+--
+--  This package is the implementation of the Transforme package
+--
+--===========================================================================
+--
+--  Copyright 2021 (C) Holger Rodriguez
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
+
 package body SH1107.Transformer is
 
    --========================================================================
@@ -15,6 +26,8 @@ package body SH1107.Transformer is
    function Get_Column_Down (X : Natural) return Natural;
    function Get_Column_Left (Y : Natural) return Natural;
 
+   --------------------------------------------------------------------------
+   --  see .ads
    function Get_Byte_Index (O    : SH1107_Orientation;
                             X, Y : Natural) return Natural is
    begin
@@ -30,41 +43,73 @@ package body SH1107.Transformer is
       end case;
    end Get_Byte_Index;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Up, this means, that the Y-coordinates
+   --  map from a logical perspective:
+   --  increasing Y coordinates are mapped to increasing pages
    function Get_Page_Up (Y : Natural) return Natural is
    begin
       return Y / 8;
    end Get_Page_Up;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Right, this means, that the X-coordinates
+   --  map from a logical perspective:
+   --  increasing X coordinates are mapped to decreasing pages
    function Get_Page_Right (X : Natural) return Natural is
    begin
       return 15 - X / 8;
    end Get_Page_Right;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Down, this means, that the Y-coordinates
+   --  map from a logical perspective:
+   --  increasing Y coordinates are mapped to decreasing pages
    function Get_Page_Down (Y : Natural) return Natural is
    begin
       return 15 - Y / 8;
    end Get_Page_Down;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Left, this means, that the X-coordinates
+   --  map from a logical perspective:
+   --  increasing X coordinates are mapped to increasing pages
    function Get_Page_Left (X : Natural) return Natural is
    begin
       return X / 8;
    end Get_Page_Left;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Up, this means, that the X-coordinates
+   --  map from a logical perspective:
+   --  increasing X coordinates are mapped to increasing columns
    function Get_Column_Up (X : Natural) return Natural is
    begin
       return X;
    end Get_Column_Up;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Right, this means, that the Y-coordinates
+   --  map from a logical perspective:
+   --  increasing Y coordinates are mapped to increasing columns
    function Get_Column_Right (Y : Natural) return Natural is
    begin
       return Y;
    end Get_Column_Right;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Down, this means, that the X-coordinates
+   --  map from a logical perspective:
+   --  increasing x coordinates are mapped to decreasing columns
    function Get_Column_Down (X : Natural) return Natural is
    begin
       return 127 - X;
    end Get_Column_Down;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Left, this means, that the Y-coordinates
+   --  map from a logical perspective:
+   --  increasing Y coordinates are mapped to decreasing columns
    function Get_Column_Left (Y : Natural) return Natural is
    begin
       return 127 - Y;
@@ -80,6 +125,8 @@ package body SH1107.Transformer is
    function Bit_Mask_Down (Y : Natural) return HAL.UInt8;
    function Bit_Mask_Left (X : Natural) return HAL.UInt8;
 
+   --------------------------------------------------------------------------
+   --  see .ads
    function Get_Bit_Mask (O    : SH1107_Orientation;
                           X, Y : Natural) return HAL.UInt8 is
    begin
@@ -96,39 +143,47 @@ package body SH1107.Transformer is
    end Get_Bit_Mask;
 
    --------------------------------------------------------------------------
+   --  LUT for bits moving from D0 to D7 in a byte
+   LUT_DO_D7 : constant HAL.UInt8_Array (0 .. 7) := (
+                                                     0 => 16#01#,
+                                                     1 => 16#02#,
+                                                     2 => 16#04#,
+                                                     3 => 16#08#,
+                                                     4 => 16#10#,
+                                                     5 => 16#20#,
+                                                     6 => 16#40#,
+                                                     7 => 16#80#
+                                                    );
+
+   --------------------------------------------------------------------------
+   --  LUT for bits moving from D7 to D0 in a byte
+   LUT_D7_D0 : constant HAL.UInt8_Array (0 .. 7) := (
+                                                     0 => 16#80#,
+                                                     1 => 16#40#,
+                                                     2 => 16#20#,
+                                                     3 => 16#10#,
+                                                     4 => 16#08#,
+                                                     5 => 16#04#,
+                                                     6 => 16#02#,
+                                                     7 => 16#01#
+                                                    );
+
+   --------------------------------------------------------------------------
    --  If the orienation is Up, this means, that the Y-coordinates
    --  map from a logical perspective:
-   --  increasing Y coordincates moves the bit mask from D0 -> D7
+   --  increasing Y coordinates moves the bit mask from D0 -> D7
    function Bit_Mask_Up (Y : Natural) return HAL.UInt8 is
-      LUT : constant HAL.UInt8_Array (0 .. 7)
-        := (
-            0 => 16#01#,
-            1 => 16#02#,
-            2 => 16#04#,
-            3 => 16#08#,
-            4 => 16#10#,
-            5 => 16#20#,
-            6 => 16#40#,
-            7 => 16#80#
-           );
    begin
-      return LUT (Y mod 8);
+      return LUT_DO_D7 (Y mod 8);
    end Bit_Mask_Up;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Right, this means, that the X-coordinates
+   --  map from a logical perspective:
+   --  increasing X coordinates moves the bit mask from D7 -> D0
    function Bit_Mask_Right (X : Natural) return HAL.UInt8 is
-      LUT : constant HAL.UInt8_Array (0 .. 7)
-        := (
-            0 => 16#80#,
-            1 => 16#40#,
-            2 => 16#20#,
-            3 => 16#10#,
-            4 => 16#08#,
-            5 => 16#04#,
-            6 => 16#02#,
-            7 => 16#01#
-           );
    begin
-      return LUT (X mod 8);
+      return LUT_D7_D0 (X mod 8);
    end Bit_Mask_Right;
 
    --------------------------------------------------------------------------
@@ -136,35 +191,17 @@ package body SH1107.Transformer is
    --  map from a logical perspective:
    --  increasing Y coordincates moves the bit mask from D7 -> D0
    function Bit_Mask_Down (Y : Natural) return HAL.UInt8 is
-      LUT : constant HAL.UInt8_Array (0 .. 7)
-        := (
-            0 => 16#80#,
-            1 => 16#40#,
-            2 => 16#20#,
-            3 => 16#10#,
-            4 => 16#08#,
-            5 => 16#04#,
-            6 => 16#02#,
-            7 => 16#01#
-           );
    begin
-      return LUT (Y mod 8);
+      return LUT_D7_D0 (Y mod 8);
    end Bit_Mask_Down;
 
+   --------------------------------------------------------------------------
+   --  If the orienation is Left, this means, that the X-coordinates
+   --  map from a logical perspective:
+   --  increasing X coordinates moves the bit mask from D0 -> D7
    function Bit_Mask_Left (X : Natural) return HAL.UInt8 is
-      LUT : constant HAL.UInt8_Array (0 .. 7)
-        := (
-            0 => 16#01#,
-            1 => 16#02#,
-            2 => 16#04#,
-            3 => 16#08#,
-            4 => 16#10#,
-            5 => 16#20#,
-            6 => 16#40#,
-            7 => 16#80#
-           );
    begin
-      return LUT (X mod 8);
+      return LUT_DO_D7 (X mod 8);
    end Bit_Mask_Left;
 
 end SH1107.Transformer;
